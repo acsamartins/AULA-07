4b0a238de64b7265c20cb104bf4db17d666132cb
Acsa <acsa.cordeiro@estudante.ifro.edu.br>
2025-10-01 21:53:23 -0400

    primeiro commit

---
+++ AULA-07/Carro.cs
@@ -0,0 +1,85 @@
+﻿public class Carro
+{
+    public int Id;
+    public string marca;
+    public string modelo;
+    private string Placa;
+    public string renavan;
+    private string chassi;
+
+
+
+
+
+
+    public void Setplaca(string placa)
+    {
+        if (char.IsLetter(placa[0]) &&
+            char.IsLetter(placa[1]) &&
+            char.IsLetter(placa[2]) &&
+            char.IsNumber(placa[3]) && char.IsNumber(placa[2]) &&
+       char.IsNumber(placa[3]))
+
+        {
+            Console.WriteLine("teste");
+            Placa = placa;
+        }
+        else
+        {
+            throw new Exception("Placa ivalida");
+        }
+
+
+
+    }
+
+
+    public string Getplaca()
+    {
+        return Placa;
+    }
+
+
+
+    public void SetChassi(string Chassi) /// colocar o atributo em private
+    {
+        chassi = chassi.ToLower();
+        if (chassi.Length != 17 && !chassi.Contains("O") && !chassi.Contains("I") && !chassi.Contains("q")) ;
+
+        {
+            throw new Exception("Chassi inválido");
+        }
+        chassi = Chassi;
+    }
+
+    public string GetChassi()
+    {
+        return chassi;
+    }
+
+    public Carro(string chassi)
+    {
+        SetChassi(chassi);
+    }
+
+
+    public Carro(int id, string Marca, string Modelo, string Placa, string Renavan, string Chassi)
+    {
+        SetChassi(chassi);
+        Id = id;
+        marca = Marca;
+        modelo = Modelo;
+        this.Placa = Placa;
+        renavan = Renavan;
+        chassi = Chassi;
+    }
+
+
+
+
+
+
+
+
+
+}
---
+++ AULA-07/Program.cs
@@ -0,0 +1,19 @@
+﻿try
+{
+
+    Carro c1 = new Carro("4587945698753695");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+Carro c2 = new Carro(1, "Ford", "ForD k", "45966555", "4445dd", "5545455");
+
+Console.WriteLine(c2.GetChassi());
+
+string a = "1";
+if (char.IsNumber(a[0]))
+{
+    Console.WriteLine();
+}
