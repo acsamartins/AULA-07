ad6e5cb61581eb2037a67bd56d83964fa60541f8
acsamartins <acsa.cordeiro@estudante.ifro.edu.br>
2025-10-01 21:59:35 -0400

    Update Carro.cs

--- AULA-07/Carro.cs
+++ AULA-07/Carro.cs
@@ -17,8 +17,11 @@
         if (char.IsLetter(placa[0]) &&
             char.IsLetter(placa[1]) &&
             char.IsLetter(placa[2]) &&
-            char.IsNumber(placa[3]) && char.IsNumber(placa[2]) &&
-       char.IsNumber(placa[3]))
+            char.IsNumber(placa[3]) &&
+            char.IsNumber(placa[4]) && || char.isLetter(placa[4])) &&
+            char.IsNumber(placa[5]  &&
+            char.IsNumber(placa[6])
+                         )
 
         {
             Console.WriteLine("teste");
